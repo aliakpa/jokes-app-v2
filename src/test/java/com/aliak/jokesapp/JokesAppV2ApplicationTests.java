768b4fab577dd03506115929ae4dc6d88babaf97
Ali Ak <aliakbartin@gmail.com>
2022-12-31 10:50:59 -0500

    Initial commit

---
+++ src/test/java/com/aliak/jokesapp/JokesAppV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aliak.jokesapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JokesAppV2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
